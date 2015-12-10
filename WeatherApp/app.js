80a9eb788997c70ad5e7189c4652b51ba0e7364d
Rohan Paul <rohanpaul2@gmail.com>
2015-12-10 17:50:03 +0530

    small change

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,5 +1,12 @@
 var weather = require('./weather.js');
+var location = require('./location.js');
 
 weather(function(currentWeather) {
     console.log(currentWeather);
 });
+
+location(function(currentLocation) {
+   console.log('City: ' + location.city);
+   console.log('log/lat' + location.loc);
+});
+
